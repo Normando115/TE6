f2ef0caad2c4332146f61dd48209e9315e4eb919
Normando115 <49627247+Normando115@users.noreply.github.com>
2022-10-28 20:54:10 -0700

    3 js files + html
    Done

---
+++ chessboard.js
@@ -0,0 +1,15 @@
+var chessboard = '';
+
+for (var i = 0; i < 8; i ++) {
+  for (var j = 0; j < 8; j ++) {
+    if ((i+j) % 2 == 0)
+    {
+      chessboard += '#';
+    }
+    else{
+      chessboard +=' ';
+    }
+  }
+  chessboard += "\n";
+}
+console.log(chessboard);
